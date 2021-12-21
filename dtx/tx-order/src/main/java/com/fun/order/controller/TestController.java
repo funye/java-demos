da91b91bed2f5b2f08da0360a6fde2cb7df5549a
funye <913510024@qq.com>
2021-12-21 17:00:26 +0800

    spring event

--- dtx/tx-order/src/main/java/com/fun/order/controller/TestController.java
+++ dtx/tx-order/src/main/java/com/fun/order/controller/TestController.java
@@ -4,10 +4,12 @@ import com.fun.order.api.OrderApi;
 import com.fun.order.api.OrderDto;
 import com.fun.order.model.Order;
 import com.fun.order.common.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
@@ -29,6 +31,7 @@ public class TestController {
     public Response<Order> testOrder() {
         Order order = new Order();
         order.setOrderNo("orderno-1000001");
+        log.info("test={}", order.getOrderNo());
         return Response.ok(order);
     }
 }
--- little-demo/src/main/java/com/fun/spring/ioc/RunTest.java
+++ little-demo/src/main/java/com/fun/spring/ioc/RunTest.java
@@ -1,20 +1,35 @@
 package com.fun.spring.ioc;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 public class RunTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
 
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beanFactoryTest.xml");
         TestBean bean = (TestBean) ctx.getBean("testBean");
         bean.sayHello();
+        bean.setName("hello bean");
 
-        long deta = (269195788498969600L-269133033127995904L)/6;
-        System.out.println(deta);
-        for (int i=0; i<10;i++) {
-            System.out.println(269133033127995904L+ i*deta);
-        }
+        Map<String,Object> properties = Maps.newHashMap();
+        properties.put("address","浙江杭州");
+        properties.put("age",26);
+        Object newBean = ReflectUtil.getTarget(bean,properties);
+        System.out.println(JSON.toJSONString(newBean));
+
+        System.out.println("string=====" + isUserClass(new String("111")));
+        System.out.println("TestBean======" + isUserClass(bean));
+        Object newBean2 = ReflectUtil.getTarget(new String("1111"),properties); // error
+        System.out.println(JSON.toJSONString(newBean2));
 
     }
+
+    public static boolean isUserClass(Object obj) {
+        return obj != null && obj.getClass().getClassLoader() != null;
+    }
+
 }
